5ef7210d63ca8668692b990564635131e83852d4
EL Ghifari <ghifariakun@gmail.com>
2024-06-10 00:22:51 +0700

    acak ayat per 1 ,5,10 juz

--- app/Http/Controllers/QuranController.php
+++ app/Http/Controllers/QuranController.php
@@ -14,6 +14,7 @@ class QuranController extends Controller
         $this->quranService = $quranService;
     }
 
+    // GET SURAT BY NUMBER
     public function getSuratByNumber($number)
     {
         $surat = $this->quranService->getSuratByNumber($number);
@@ -25,6 +26,7 @@ class QuranController extends Controller
         return response()->json($surat);
     }
 
+    // GET ALL SURAT
     public function getAllSurat()
     {
         $surat = $this->quranService->getAllSurat();
@@ -36,6 +38,7 @@ class QuranController extends Controller
         return response()->json($surat);
     }
 
+    // GET AYAT BY INDEX
     public function getAyatByIndex($index)
     {
         $ayat = $this->quranService->getAyatByIndex($index);
@@ -47,6 +50,260 @@ class QuranController extends Controller
         return response()->json($ayat);
     }
 
+    ////////////////////////////////////////////////////////////////////
+
+    // GET ACAK AYAT BY JUZ
+    private function getAcakAyat($start, $end)
+    {
+        $acak = rand($start, $end);
+        $ayat = $this->quranService->getAyatByIndex($acak);
+
+        if (!$ayat) {
+            return response()->json(['error' => 'Ayat not found'], 404);
+        }
+
+        return response()->json($ayat);
+    }
+    // GET ACAK JUZ 1
+    public function getAcakJuz1()
+    {
+        return $this->getAcakAyat(1, 148);
+    }
+
+    // GET ACAK JUZ 2
+    public function getAcakJuz2()
+    {
+        return $this->getAcakAyat(149, 259);
+    }
+
+    // GET ACAK JUZ 3
+    public function getAcakJuz3()
+    {
+        return $this->getAcakAyat(260, 384);
+    }
+
+    // GET ACAK JUZ 4
+    public function getAcakJuz4()
+    {
+        return $this->getAcakAyat(385, 516);
+    }
+
+    // GET ACAK JUZ 5
+    public function getAcakJuz5()
+    {
+        return $this->getAcakAyat(517, 640);
+    }
+
+    // GET ACAK JUZ 6
+    public function getAcakJuz6()
+    {
+        return $this->getAcakAyat(641, 751);
+    }
+
+    // GET ACAK JUZ 7
+    public function getAcakJuz7()
+    {
+        return $this->getAcakAyat(752, 899);
+    }
+
+    // GET ACAK JUZ 8
+    public function getAcakJuz8()
+    {
+        return $this->getAcakAyat(900, 1041);
+    }
+
+    // GET ACAK JUZ 9
+    public function getAcakJuz9()
+    {
+        return $this->getAcakAyat(1042, 1200);
+    }
+
+    // GET ACAK JUZ 10
+    public function getAcakJuz10()
+    {
+        return $this->getAcakAyat(1201, 1328);
+    }
+
+    // GET ACAK JUZ 11
+    public function getAcakJuz11()
+    {
+        return $this->getAcakAyat(1329, 1478);
+    }
+
+    // GET ACAK JUZ 12
+    public function getAcakJuz12()
+    {
+        return $this->getAcakAyat(1479, 1648);
+    }
+
+    // GET ACAK JUZ 13
+    public function getAcakJuz13()
+    {
+        return $this->getAcakAyat(1649, 1803);
+    }
+
+    // GET ACAK JUZ 14
+    public function getAcakJuz14()
+    {
+        return $this->getAcakAyat(1804, 2029);
+    }
+
+    // GET ACAK JUZ 15
+    public function getAcakJuz15()
+    {
+        return $this->getAcakAyat(2030, 2214);
+    }
+
+    // GET ACAK JUZ 16
+    public function getAcakJuz16()
+    {
+        return $this->getAcakAyat(2215, 2483);
+    }
+
+    // GET ACAK JUZ 17
+    public function getAcakJuz17()
+    {
+        return $this->getAcakAyat(2484, 2673);
+    }
+
+    // GET ACAK JUZ 18
+    public function getAcakJuz18()
+    {
+        return $this->getAcakAyat(2674, 2875);
+    }
+
+    // GET ACAK JUZ 19
+    public function getAcakJuz19()
+    {
+        return $this->getAcakAyat(2876, 3218);
+    }
+
+    // GET ACAK JUZ 20
+    public function getAcakJuz20()
+    {
+        return $this->getAcakAyat(3219, 3384);
+    }
+
+    // GET ACAK JUZ 21
+    public function getAcakJuz21()
+    {
+        return $this->getAcakAyat(3385, 3563);
+    }
+
+    // GET ACAK JUZ 22
+    public function getAcakJuz22()
+    {
+        return $this->getAcakAyat(3564, 3726);
+    }
+
+    // GET ACAK JUZ 23
+    public function getAcakJuz23()
+    {
+        return $this->getAcakAyat(3727, 4089);
+    }
+
+    // GET ACAK JUZ 24
+    public function getAcakJuz24()
+    {
+        return $this->getAcakAyat(4090, 4264);
+    }
+
+    // GET ACAK JUZ 25
+    public function getAcakJuz25()
+    {
+        return $this->getAcakAyat(4265, 4510);
+    }
+
+    // GET ACAK JUZ 26
+    public function getAcakJuz26()
+    {
+        return $this->getAcakAyat(4511, 4705);
+    }
+
+    // GET ACAK JUZ 27
+    public function getAcakJuz27()
+    {
+        return $this->getAcakAyat(4706, 5104);
+    }
+
+    // GET ACAK JUZ 28
+    public function getAcakJuz28()
+    {
+        return $this->getAcakAyat(5105, 5241);
+    }
+
+    // GET ACAK JUZ 29
+    public function getAcakJuz29()
+    {
+        return $this->getAcakAyat(5242, 5672);
+    }
+
+    // GET ACAK JUZ 30
+    public function getAcakJuz30()
+    {
+        return $this->getAcakAyat(5673, 6236);
+    }
+
+/////////////////////////////////   ACAK PER 5 JUZ
+
+    // GET ACAK JUZ 1 - 5
+    public function getAcakJuz1Juz5()
+    {
+        return $this->getAcakAyat(1, 640);
+    }
+
+    // GET ACAK JUZ 6 - 10
+    public function getAcakJuz6Juz10()
+    {
+        return $this->getAcakAyat(641, 1328);
+    }
+
+    // GET ACAK JUZ 11 - 15
+    public function getAcakJuz11Juz15()
+    {
+        return $this->getAcakAyat(1329, 2214);
+    }
+
+    // GET ACAK JUZ 16 - 20
+    public function getAcakJuz16Juz20()
+    {
+        return $this->getAcakAyat(2215, 3384);
+    }
+
+    // GET ACAK JUZ 21 - 25
+    public function getAcakJuz21Juz25()
+    {
+        return $this->getAcakAyat(3385, 4510);
+    }
+
+    // GET ACAK JUZ 26 - 30
+    public function getAcakJuz26Juz30()
+    {
+        return $this->getAcakAyat(4511, 6236);
+    }
+
+    /////////////////////////////////   ACAK PER 10 JUZ
+
+    // GET ACAK JUZ 1 - 10
+    public function getAcakJuz1Juz10()
+    {
+        return $this->getAcakAyat(1, 1328);
+    }
+
+    // GET ACAK JUZ 11 - 20
+    public function getAcakJuz11Juz20()
+    {
+        return $this->getAcakAyat(1329, 3384);
+    }
+
+    // GET ACAK JUZ 21 - 30
+    public function getAcakJuz21Juz30()
+    {
+        return $this->getAcakAyat(3385, 6236);
+    }
+
+
+    // GET AYAT BY SURAT AND AYAT
     public function getAyatBySuratAndAyat($surat, $ayat)
     {
         $ayat = $this->quranService->getAyatBySuratAndAyat($surat, $ayat);
@@ -58,6 +315,7 @@ class QuranController extends Controller
         return response()->json($ayat);
     }
 
+    // GET AYAT RANGE
     public function getAyatRange($surat, $ayat, $panjang)
     {
         $ayatRange = $this->quranService->getAyatRange($surat, $ayat, $panjang);
@@ -69,6 +327,7 @@ class QuranController extends Controller
         return response()->json($ayatRange);
     }
 
+    // GET AYAT BY PAGE
     public function getAyatByPage($page)
     {
         $ayatPage = $this->quranService->getAyatByPage($page);
@@ -80,6 +339,7 @@ class QuranController extends Controller
         return response()->json($ayatPage);
     }
 
+    // GET AYAT BY JUZ
     public function getAyatByJuz($juz)
     {
         $ayatJuz = $this->quranService->getAyatByJuz($juz);
@@ -89,6 +349,7 @@ class QuranController extends Controller
         return response()->json($ayatJuz);
     }
 
+    // GET ALL JUZ
     public function getAllJuz()
     {
         $juz = $this->quranService->getAllJuz();
@@ -98,15 +359,17 @@ class QuranController extends Controller
         return response()->json($juz);
     }
 
+    // GET AYAT BY TEMA
     public function getAyatByTema($tema)
-{
-    $ayat = $this->quranService->getAyatByTema($tema);
-    if (!$ayat) {
-        return response()->json(['error' => 'Ayat not found for the given tema'], 404);
-    }
+    {
+        $ayat = $this->quranService->getAyatByTema($tema);
+        if (!$ayat) {
+            return response()->json(['error' => 'Ayat not found for the given tema'], 404);
+        }
         return response()->json($ayat);
     }
 
+    // GET ALL TEMA
     public function getAllTema()
     {
         $tema = $this->quranService->getAllTema();
@@ -115,6 +378,4 @@ class QuranController extends Controller
         }
         return response()->json($tema);
     }
-
-
 }
--- routes/api.php
+++ routes/api.php
@@ -11,16 +11,16 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 // > https://muslimapp.elghifari.site/api/prayer-times/jakarta/2024-05-27
-Route::get('prayer-times/{location}/{date}', [PrayerTimeController::class, 'index']);
+Route::get('prayertimes/{location}/{date}', [PrayerTimeController::class, 'index']);
 
 // > https://muslimapp.elghifari.site/api/quran/surat/25
-Route::get('/quran-surat/{number}', [QuranController::class, 'getSuratByNumber']);
+Route::get('/quransurat/{number}', [QuranController::class, 'getSuratByNumber']);
 
 // > https://muslimapp.elghifari.site/api/quran-surat/semua
-Route::get('/quran-surat/semua', [QuranController::class, 'getAllSurat']);
+Route::get('/quransurat/semua', [QuranController::class, 'getAllSurat']);
 
 // > https://muslimapp.elghifari.site/api/quran-ayat/1
-Route::get('/quran-ayat/{index}', [QuranController::class, 'getAyatByIndex']);
+Route::get('/quranayat/{index}', [QuranController::class, 'getAyatByIndex']);
 
 // > https://muslimapp.elghifari.site/api/quran/25/1
 Route::get('/quran/{surat}/{ayat}', [QuranController::class, 'getAyatBySuratAndAyat']);
@@ -35,13 +35,138 @@ Route::get('/quran/page/{page}', [QuranController::class, 'getAyatByPage']);
 Route::get('/quran/juz/{juz}', [QuranController::class, 'getAyatByJuz']);
 
 // > https://muslimapp.elghifari.site/api/quran/juz/semua
-Route::get('/quran-juz/semua', [QuranController::class, 'getAllJuz']);
+Route::get('/quranjuz/semua', [QuranController::class, 'getAllJuz']);
 
-// > https://muslimapp.elghifari.site/api/quran-kisah/1
-Route::get('/quran-kisah/{tema}', [QuranController::class, 'getAyatByTema']);
+////////////////////////////////////////////////////   ACAK PER JUZ
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz1
+Route::get('/quran/ayat/acak/juz1', [QuranController::class, 'getAcakJuz1']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz2
+Route::get('/quran/ayat/acak/juz2', [QuranController::class, 'getAcakJuz2']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz3
+Route::get('/quran/ayat/acak/juz3', [QuranController::class, 'getAcakJuz3']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz4
+Route::get('/quran/ayat/acak/juz4', [QuranController::class, 'getAcakJuz4']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz5
+Route::get('/quran/ayat/acak/juz5', [QuranController::class, 'getAcakJuz5']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz6
+Route::get('/quran/ayat/acak/juz6', [QuranController::class, 'getAcakJuz6']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz7
+Route::get('/quran/ayat/acak/juz7', [QuranController::class, 'getAcakJuz7']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz8
+Route::get('/quran/ayat/acak/juz8', [QuranController::class, 'getAcakJuz8']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz9
+Route::get('/quran/ayat/acak/juz9', [QuranController::class, 'getAcakJuz9']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz10
+Route::get('/quran/ayat/acak/juz10', [QuranController::class, 'getAcakJuz10']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz11
+Route::get('/quran/ayat/acak/juz11', [QuranController::class, 'getAcakJuz11']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz12
+Route::get('/quran/ayat/acak/juz12', [QuranController::class, 'getAcakJuz12']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz13
+Route::get('/quran/ayat/acak/juz13', [QuranController::class, 'getAcakJuz13']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz14
+Route::get('/quran/ayat/acak/juz14', [QuranController::class, 'getAcakJuz14']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz15
+Route::get('/quran/ayat/acak/juz15', [QuranController::class, 'getAcakJuz15']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz16
+Route::get('/quran/ayat/acak/juz16', [QuranController::class, 'getAcakJuz16']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz17
+Route::get('/quran/ayat/acak/juz17', [QuranController::class, 'getAcakJuz17']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz18
+Route::get('/quran/ayat/acak/juz18', [QuranController::class, 'getAcakJuz18']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz19
+Route::get('/quran/ayat/acak/juz19', [QuranController::class, 'getAcakJuz19']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz20
+Route::get('/quran/ayat/acak/juz20', [QuranController::class, 'getAcakJuz20']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz21
+Route::get('/quran/ayat/acak/juz21', [QuranController::class, 'getAcakJuz21']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz22
+Route::get('/quran/ayat/acak/juz22', [QuranController::class, 'getAcakJuz22']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz23
+Route::get('/quran/ayat/acak/juz23', [QuranController::class, 'getAcakJuz23']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz24
+Route::get('/quran/ayat/acak/juz24', [QuranController::class, 'getAcakJuz24']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz25
+Route::get('/quran/ayat/acak/juz25', [QuranController::class, 'getAcakJuz25']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz26
+Route::get('/quran/ayat/acak/juz26', [QuranController::class, 'getAcakJuz26']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz27
+Route::get('/quran/ayat/acak/juz27', [QuranController::class, 'getAcakJuz27']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz28
+Route::get('/quran/ayat/acak/juz28', [QuranController::class, 'getAcakJuz28']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz29
+Route::get('/quran/ayat/acak/juz29', [QuranController::class, 'getAcakJuz29']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz30
+Route::get('/quran/ayat/acak/juz30', [QuranController::class, 'getAcakJuz30']);
+
+////////////////////////////////////////////////////   ACAK PER 5 JUZ
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz1-5
+Route::get('/quran/ayat/acak/juz1-5', [QuranController::class, 'getAcakJuz1Juz5']);
+
+// > https://muslimapp.elghifari.site/api//quran/ayat/acak/juz6-10
+Route::get('/quran/ayat/acak/juz6-10', [QuranController::class, 'getAcakJuz6Juz10']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz11-15
+Route::get('/quran/ayat/acak/juz11-15', [QuranController::class, 'getAcakJuz11Juz15']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz16-20
+Route::get('/quran/ayat/acak/juz16-20', [QuranController::class, 'getAcakJuz16Juz20']);
+
+// > https://muslimapp.elghifari.site/api//quran/ayat/acak/juz21-25
+Route::get('/quran/ayat/acak/juz21-25', [QuranController::class, 'getAcakJuz21Juz25']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz26-30
+Route::get('/quran/ayat/acak/juz26-30', [QuranController::class, 'getAcakJuz26Juz30']);
+
+////////////////////////////////////////////////////   ACAK PER 10 JUZ
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz1-10
+Route::get('/quran/ayat/acak/juz1-10', [QuranController::class, 'getAcakJuz1Juz10']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz11-20
+Route::get('/quran/ayat/acak/juz11-20', [QuranController::class, 'getAcakJuz11Juz20']);
+
+// > https://muslimapp.elghifari.site/api/quran/ayat/acak/juz21-30
+Route::get('/quran/ayat/acak/juz21-30', [QuranController::class, 'getAcakJuz21Juz30']);
+
+/////////////////////////////////////////////////////////////////////////////
+
+// > https://muslimapp.elghifari.site/api/quran/kisah/1
+Route::get('/qurankisah/{tema}', [QuranController::class, 'getAyatByTema']);
 
 // > https://muslimapp.elghifari.site/api/quran-semua-kisah
-Route::get('quran-semua-kisah', [QuranController::class, 'getAllTema']);
+Route::get('qurankisahsemua', [QuranController::class, 'getAllTema']);
 
 
 
